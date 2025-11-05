860debb0872241f71070fc9d6b354688ca06a35d
PranavShadow <ompranav2003@gmail.com>
2025-11-06 04:26:55 +0530

    base v1.1

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header.jsx"
+import { ConvexClientProvider } from "@/components/ConvexClientProvider.jsx";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata = {
   title: "TransactBud",
@@ -20,12 +15,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link/>
+        <link rel="icon" href="/logos/logo-main.png"/>
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${inter.className} `}>
+        <ClerkProvider>
+        <ConvexClientProvider>
+          <Header/>
+        <main className="min-h-screen">
+          {children}
+        </main>
+        </ConvexClientProvider>
+        </ClerkProvider>
+        
       </body>
     </html>
   );
