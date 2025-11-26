de3f63b03d40bae342c20e312540bfcc1bd54166
PranavShadow <ompranav2003@gmail.com>
2025-11-27 00:42:04 +0530

    base v1.3

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+// layout files for each next.js folder is common to all the files in the next.js folder
 const AuthLayout = ({children}) => {
   return (
     <div className='flex justify-center pt-40'>
